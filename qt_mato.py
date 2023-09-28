4497709895f17bbcb8517a76f27dc3eb9006ae82
k2300507 <elina.knuuttila@seamk.fi>
2023-09-28 17:20:08 +0300

    Ruuan syönti

--- qt_mato.py
+++ qt_mato.py
@@ -49,7 +49,11 @@ class SnakeGame(QGraphicsView):
             new_head = (head_x, head_y + 1)
 
         self.snake.insert(0, new_head)
-        self.snake.pop()
+        
+        if new_head == self.food:
+            self.food = self.spawn_food()
+        else:
+            self.snake.pop()
 
         self.print_game()
 
